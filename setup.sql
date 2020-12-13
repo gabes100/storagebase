36d473f68b2590b67ceb694290f69885924b27d8
gabe <gabesv100@gmail.com>
2020-12-13 15:59:17 -0600

    sql changed

--- setup.sql
+++ setup.sql
@@ -1,16 +1,16 @@
+DROP DATABASE storagebase;
+
 create database storagebase;
 use storagebase;
 
-create user 'default'@'localhost' identified by 'password';
-grant all on default.* to 'default'@'localhost';
 
 create table User (
-  id INT AUTO_INCREMENT,
-  username VARCHAR(255) NOT NULL,
-  firstname VARCHAR(255) NOT NULL,
-  lastname VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL, 
-  PRIMARY KEY (id)
+	id INT AUTO_INCREMENT,
+	firstname VARCHAR(255) NOT NULL,
+	lastname VARCHAR(255) NOT NULL,
+	username VARCHAR(255) NOT NULL,
+	password VARCHAR(255) NOT NULL,
+	PRIMARY KEY (id)
 );
 
 create table GroceryOrder (
@@ -42,5 +42,33 @@ create table StorageUnit (
   storageType VARCHAR(255) NOT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (userId) REFERENCES User(id)
-)`;
+);
 
+INSERT INTO User
+VALUES(1, 'Gabriel', 'Veldboom', 'gabriel.veldboom', '$2b$10$ORP3AYeSQA0tCXHV1sQQhOV4//0mlPDnEVvQTov/qOHDi3EuV7Svy');
+
+INSERT INTO GroceryOrder VALUES
+(1, 1, 'First Order', '2020-12-14', 11.00, 29.94),
+(2, 1, 'Second Order', '2020-12-14', 40.00, 146.44);
+
+
+INSERT INTO Item VALUES
+(1, 1, 'Eggs', '2021-01-14', 2.99, 'Dairy', 2),
+(2, 1, 'Ice Cream Snack', '2021-01-16', 0.99, 'Dairy', 1),
+(3, 1, 'Frozen Pizza', '2021-02-12', 2.00, 'Other', 5),
+(4, 1, 'Bread', '2021-01-16', 2.99, 'Grain', 1),
+(5, 1, 'Beef (1 pound)', '2021-01-16', 5.99, 'Meat', 1),
+(6, 1, 'Butter', '2021-01-16', 3.99, 'Dairy', 1),
+(7, 2, 'Sour Cream', '2020-12-31', 3.99, 'Dairy', 1),
+(8, 2, 'Corn Flakes', '2022-04-12', 4.99, 'Grain', 3),
+(9, 2, 'Mac And Chese', '2024-01-01', 0.99, 'Grain', 5),
+(10, 2, 'Peanut Butter', '2021-05-04', 3.46, 'Other', 1),
+(11, 2, 'Jelly', '2021-01-16', 2.00, 'Other', 1),
+(12, 2, 'Chocholate Chips', '2022-03-15', 3.99, 'Other', 3),
+(13, 2, 'Noodles', '2021-01-26', 5.99, 'Grain', 4),
+(14, 2, 'Salt', '2021-01-05', 1.29, 'Other', 1),
+(15, 2, 'Chicken', '2021-01-20', 5.99, 'Meat',10),
+(16, 2, 'Waffles', '2022-07-16', 4.99, 'Grain', 2),
+(17, 2, 'Frozen Peas', '2021-01-16',3.99, 'Vegetable', 2),
+(18, 2, 'Creamed Corn', '2021-01-15', 2.99, 'Vegetable,', 5),
+(19, 2, 'Chicken Noodle Soup', '2021-01-16', 0.99, 'Other', 2);
